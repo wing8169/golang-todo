816b0f509d8776bde9340228f745c8e2f5fcf31d
Jia Xiong <wing8169@users.noreply.github.com>
2023-11-05 23:16:39 +0800

    Setup environment

--- main.go
+++ main.go
@@ -1,3 +1,19 @@
+package main
+
+import (
+	"context"
+
+	"github.com/labstack/echo/v4"
+	"github.com/wing8169/golang-todo/templates"
+)
+
 func main() {
-  fmt.Println("hi")
+	e := echo.New()
+	// Main menu
+	component := templates.Hello("John")
+	e.GET("/", func(c echo.Context) error {
+		return component.Render(context.Background(), c.Response().Writer)
+	})
+	e.Static("/static", "static")
+	e.Logger.Fatal(e.Start(":3000"))
 }
