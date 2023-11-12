53d1adb4b23378ad8d36e1717eadc3bd3275cbc6
Jia Xiong <chinxiongwei@yahoo.com.my>
2023-11-12 16:36:50 +0800

    Integrate edit operation

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"net/http"
 
 	"github.com/google/uuid"
@@ -19,6 +20,15 @@ func filterByID(todos []*dto.TodoCardDto, id string) (out []*dto.TodoCardDto) {
 		out = append(out, todo)
 	}
 	return out
+}
+
+func findByID(todos []*dto.TodoCardDto, id string) *dto.TodoCardDto {
+	for _, todo := range todos {
+		if todo.ID == id {
+			return todo
+		}
+	}
+	return nil
 }
 
 func main() {
@@ -55,6 +65,22 @@ func main() {
 		component := components.TodoCardsWithBtn(todos)
 		return component.Render(context.Background(), c.Response().Writer)
 	})
+	e.PUT("/todos/:id", func(c echo.Context) error {
+		id := c.Param("id")
+		if id == "" {
+			return echo.NewHTTPError(http.StatusBadRequest, "Invalid id")
+		}
+		todo := findByID(todos, id)
+		if todo == nil {
+			return echo.NewHTTPError(http.StatusNotFound, "Todo card not found")
+		}
+		todo.Text = c.FormValue("edit-todo-input")
+		component := components.TodoCard(*todo)
+		for _, t := range todos {
+			fmt.Println(t.Text)
+		}
+		return component.Render(context.Background(), c.Response().Writer)
+	})
 	e.DELETE("/todos/:id", func(c echo.Context) error {
 		id := c.Param("id")
 		if id == "" {
@@ -66,12 +92,21 @@ func main() {
 	})
 	e.GET("/components", func(c echo.Context) error {
 		t := c.QueryParam("type")
+		id := c.QueryParam("id")
 		switch t {
 		case "add-todo":
 			component := components.AddTodoInput()
 			return component.Render(context.Background(), c.Response().Writer)
 		case "add-todo-btn":
 			component := components.AddTodoButton()
+			return component.Render(context.Background(), c.Response().Writer)
+		case "edit-todo-input":
+			todo := findByID(todos, id)
+			component := components.EditTodoInput(todo)
+			return component.Render(context.Background(), c.Response().Writer)
+		case "edit-todo-btn":
+			todo := findByID(todos, id)
+			component := components.TodoCard(*todo)
 			return component.Render(context.Background(), c.Response().Writer)
 		}
 		return echo.NewHTTPError(http.StatusBadRequest, "Invalid element")
--- templates/components/edit_todo_input_templ.go
+++ templates/components/edit_todo_input_templ.go
@@ -10,10 +10,9 @@ import "context"
 import "io"
 import "bytes"
 
-import "github.com/wing8169/golang-todo/templates/common"
 import "github.com/wing8169/golang-todo/dto"
 
-func TodoCard(todo dto.TodoCardDto) templ.Component {
+func EditTodoInput(todo *dto.TodoCardDto) templ.Component {
 	return templ.ComponentFunc(func(ctx context.Context, templ_7745c5c3_W io.Writer) (templ_7745c5c3_Err error) {
 		templ_7745c5c3_Buffer, templ_7745c5c3_IsBuffer := templ_7745c5c3_W.(*bytes.Buffer)
 		if !templ_7745c5c3_IsBuffer {
@@ -26,48 +25,23 @@ func TodoCard(todo dto.TodoCardDto) templ.Component {
 			templ_7745c5c3_Var1 = templ.NopComponent
 		}
 		ctx = templ.ClearChildren(ctx)
-		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<div class=\"p-6 py-3 flex flex-row items-center w-full gap-4 rounded-2xl border border-black border-solid\" id=\"")
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<form class=\"flex flex-row w-full gap-4 items-center\" id=\"")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
-		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString("todo-" + todo.ID))
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString("edit-todo-" + todo.ID))
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
-		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("\">")
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("\"><input type=\"text\" name=\"edit-todo-input\" class=\"rounded-2xl text-black font-sans text-sm flex-1\" value=\"")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
-		templ_7745c5c3_Err = common.Typography("normal", todo.Text, "flex-1").Render(ctx, templ_7745c5c3_Buffer)
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(todo.Text))
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
-		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<span class=\"material-symbols-outlined cursor-pointer hover:opacity-50\" hx-get=\"")
-		if templ_7745c5c3_Err != nil {
-			return templ_7745c5c3_Err
-		}
-		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString("/components?type=edit-todo-input&id=" + todo.ID))
-		if templ_7745c5c3_Err != nil {
-			return templ_7745c5c3_Err
-		}
-		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("\" hx-trigger=\"click\" hx-swap=\"outerHTML\" hx-target=\"")
-		if templ_7745c5c3_Err != nil {
-			return templ_7745c5c3_Err
-		}
-		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString("#todo-" + todo.ID))
-		if templ_7745c5c3_Err != nil {
-			return templ_7745c5c3_Err
-		}
-		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("\">")
-		if templ_7745c5c3_Err != nil {
-			return templ_7745c5c3_Err
-		}
-		templ_7745c5c3_Var2 := `edit`
-		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ_7745c5c3_Var2)
-		if templ_7745c5c3_Err != nil {
-			return templ_7745c5c3_Err
-		}
-		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("</span> <span class=\"material-symbols-outlined cursor-pointer hover:opacity-50\" hx-delete=\"")
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("\"> <span class=\"material-symbols-outlined cursor-pointer text-black\" hx-put=\"")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
@@ -75,16 +49,49 @@ func TodoCard(todo dto.TodoCardDto) templ.Component {
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
-		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("\" hx-trigger=\"click\" hx-swap=\"outerHTML\" hx-target=\"#todos\">")
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("\" hx-trigger=\"click\" hx-swap=\"outerHTML\" hx-target=\"")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
-		templ_7745c5c3_Var3 := `delete`
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString("#edit-todo-" + todo.ID))
+		if templ_7745c5c3_Err != nil {
+			return templ_7745c5c3_Err
+		}
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("\">")
+		if templ_7745c5c3_Err != nil {
+			return templ_7745c5c3_Err
+		}
+		templ_7745c5c3_Var2 := `check`
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ_7745c5c3_Var2)
+		if templ_7745c5c3_Err != nil {
+			return templ_7745c5c3_Err
+		}
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("</span> <span class=\"material-symbols-outlined cursor-pointer text-black\" hx-get=\"")
+		if templ_7745c5c3_Err != nil {
+			return templ_7745c5c3_Err
+		}
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString("/components?type=edit-todo-btn&id=" + todo.ID))
+		if templ_7745c5c3_Err != nil {
+			return templ_7745c5c3_Err
+		}
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("\" hx-trigger=\"click\" hx-swap=\"outerHTML\" hx-target=\"")
+		if templ_7745c5c3_Err != nil {
+			return templ_7745c5c3_Err
+		}
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString("#edit-todo-" + todo.ID))
+		if templ_7745c5c3_Err != nil {
+			return templ_7745c5c3_Err
+		}
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("\">")
+		if templ_7745c5c3_Err != nil {
+			return templ_7745c5c3_Err
+		}
+		templ_7745c5c3_Var3 := `close`
 		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ_7745c5c3_Var3)
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
-		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("</span></div>")
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("</span></form>")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
